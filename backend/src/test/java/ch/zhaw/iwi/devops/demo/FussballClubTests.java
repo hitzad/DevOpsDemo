4501aced020ccc377c2f5a5ac7ba5621fec8e686
hitzad <higit config --global user.email hitzadr1@students.zhaw.ch>
2024-04-28 13:58:14 +0200

    first commit

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/FussballClubTests.java
@@ -0,0 +1,20 @@
+package ch.zhaw.iwi.devops.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class FussballClubTests {
+    @Test
+    void testGetDescription() {
+
+    }
+
+    @Test
+    void testGetId() {
+
+    }
+
+    @Test
+    void testGetTitle() {
+
+    }
+}
