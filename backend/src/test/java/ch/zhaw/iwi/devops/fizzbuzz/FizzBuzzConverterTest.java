f3817ea3d7a7fa020a9ccd4ba48a36e91abb216c
hitzad <hitzadr1@students.zhaw.ch>
2024-05-14 18:15:55 +0200

    Testfall Woche 10

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverterTest.java
@@ -7,41 +7,31 @@ public class FizzBuzzConverterTest {
 
     @Test
     public void fizzBuzzConverter1() {
-
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
         Assertions.assertEquals("1", fizzBuzz.convert(1));
-
     }
 
     @Test
     public void fizzBuzzConvertor2() {
-
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
         Assertions.assertEquals("2", fizzBuzz.convert(2));
-
     }
 
     @Test
     public void fizzBuzzConvertor3() {
-
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
         Assertions.assertEquals("Fizz", fizzBuzz.convert(3));
     }
     
     @Test
     public void fizzBuzzConvertorMultiplesOfThree() {
-
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
         Assertions.assertEquals("Fizz", fizzBuzz.convert(6));
     }
 
     @Test
     void fizzBuzzConvertorMultiplesOfSeven() {
-
         FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
-
         Assertions.assertEquals("Buzz", fizzBuzz.convert(7));
     }
 
@@ -52,6 +42,16 @@ public class FizzBuzzConverterTest {
         Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(21));
         Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(42));
         Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(63));
+    }
+
+    @Test
+    void fizzBuzzConvertorMultiplesOfFive() {
+        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+        Assertions.assertEquals("Bang", fizzBuzz.convert(5));
+        Assertions.assertEquals("Bang", fizzBuzz.convert(10));
+        Assertions.assertEquals("FizzBang", fizzBuzz.convert(15)); // Testing combination with "Fizz"
+        Assertions.assertEquals("BuzzBang", fizzBuzz.convert(35)); // Testing combination with "Buzz"
+        Assertions.assertEquals("FizzBuzzBang", fizzBuzz.convert(105)); // Testing combination with both "Fizz" and "Buzz"
     }
 
 }
