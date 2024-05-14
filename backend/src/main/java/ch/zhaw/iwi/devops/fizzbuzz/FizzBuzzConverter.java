0c83ac36492a0dcb4e4d2bfbf88c7263c70b39f0
hitzad <hitzadr1@students.zhaw.ch>
2024-05-14 17:46:24 +0200

    CODE erweiterung Week 10

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
@@ -3,12 +3,20 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 public class FizzBuzzConverter {
 
     public String convert(int i) {
-        if (i % 3 == 0 && i % 7 == 0) {
+        if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+            return "FizzBuzzBang";
+        } else if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBang";
+        } else if (i % 5 == 0 && i % 7 == 0) {
+            return "BuzzBang";
+        } else if (i % 3 == 0 && i % 7 == 0) {
             return "FizzBuzz";
         } else if (i % 3 == 0) {
             return "Fizz";
         } else if (i % 7 == 0) {
             return "Buzz";
+        } else if (i % 5 == 0) {
+            return "Bang";
         }
         return String.valueOf(i);
     }
